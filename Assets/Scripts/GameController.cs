88cd988c7e2f8e4a25877e352ac58026327e86dc
lacarreta <marcelo@lacarreta.com.br>
2022-10-04 21:19:04 -0300

    teste

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -13,7 +13,7 @@ public class GameController : MonoBehaviour
     public float VelCamera = 1;
     public float LimiteCenarios;   
     //O LACA É FODA
-
+// eh mintira
     // Start is called before the first frame update
     void Start()
     {
