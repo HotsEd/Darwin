b7f14f17c4e97cda69d62748553dc1beac8c7233
savipriscilarp <savina.pessoa@sga.pucminas.br>
2022-11-26 19:49:59 -0300

    Som itens e especial
    Consegui colocar o som dos itens e do especial

--- Assets/Scripts/ItemGenerico.cs
+++ Assets/Scripts/ItemGenerico.cs
@@ -6,28 +6,33 @@ public class ItemGenerico : MonoBehaviour
 {
     private GameController controller;
     public bool Vida = false, Especial = false;
+    private AudioSource sound;
 
 
     private void Start()
     {
         controller = GameObject.Find("GameController").GetComponent<GameController>();
+        sound = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        
         if (other.CompareTag("Player"))
         {
+            sound.Play();
+
             if(Vida == true && controller != null)
-            {
+            {           
                 controller.GanharVida();
             }
 
             if (Especial == true && controller != null)
-            {
+            {             
                 controller.GanharEspecial();
             }
-
-            Destroy(gameObject);
+           
+            Destroy(gameObject, 0.1f);
         }
     }
 }
